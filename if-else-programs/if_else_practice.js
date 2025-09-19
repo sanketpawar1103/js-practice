a944cca4123ca606ffe8b961d212eeab2193956b
Sanket Pawar <pawarss1103@gmail.com>
2025-09-19 14:36:05 +0530

    Check the flow of program in if_else block

--- if-else-programs/if_else_practice.js
+++ if-else-programs/if_else_practice.js
@@ -1,5 +1,4 @@
-// 1. Code for Checking age and giving permission 
-
+// Code for Checking age and giving permission 
 const age = 100;
 
 if (age >= 18) {
@@ -9,3 +8,13 @@ if (age >= 18) {
   console.log("You can't Drive");
   console.log("You can't Drive");
 }
+
+
+// Checking the flow of code based on decisions
+if(true) {
+  console.log("I am inside an If block ");
+} else {
+  console.log("I am inside an Else block");
+}
+
+console.log("I am outside an If/Else block");
