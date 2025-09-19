2b12136a5a287501aa6434af9b1432b68d0a79a1
Sanket Pawar <pawarss1103@gmail.com>
2025-09-19 18:16:10 +0530

    Last step of logic of factorial

---
+++ factorial-of-number/logic_of_factorial.js
@@ -0,0 +1,30 @@
+let factorialOfNumber = 1;
+let increamentingNumber = 1; 
+
+// Iteration 1 
+factorialOfNumber = increamentingNumber * factorialOfNumber;
+increamentingNumber++;
+console.log(factorialOfNumber);
+
+// Iteration 2 
+factorialOfNumber = increamentingNumber * factorialOfNumber;
+increamentingNumber++;
+console.log(factorialOfNumber);
+
+// Iteration 3 
+factorialOfNumber = increamentingNumber * factorialOfNumber;
+increamentingNumber++;
+console.log(factorialOfNumber);
+
+// Iteration 4 
+factorialOfNumber = increamentingNumber * factorialOfNumber;
+increamentingNumber++;
+console.log(factorialOfNumber);
+
+// Iteration 5 
+factorialOfNumber = increamentingNumber * factorialOfNumber;
+increamentingNumber++;
+console.log(factorialOfNumber);
+
+
+
