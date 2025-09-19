bd118feb746c9e6db2b986dc6d084c8b9a0ded18
Sanket Pawar <pawarss1103@gmail.com>
2025-09-19 14:32:29 +0530

    Check age of person and tell that, can he/she vote and drive or not.

---
+++ if-else-programs/if_else_practice.js
@@ -0,0 +1,11 @@
+// 1. Code for Checking age and giving permission 
+
+const age = 100;
+
+if (age >= 18) {
+  console.log("You can Drive");
+  console.log("You can Vote");
+} else {
+  console.log("You can't Drive");
+  console.log("You can't Drive");
+}
