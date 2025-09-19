a0276583859f4c4c8f8f4096dd7c65654e1d70cb
Sanket Pawar <pawarss1103@gmail.com>
2025-09-19 17:14:38 +0530

    Switch case practice program for the age of a person.

---
+++ switch_case_program.js
@@ -0,0 +1,17 @@
+// Switch case program for the age of a person.
+let ageOfPerson = 20;
+
+switch(ageOfPerson){
+  case 10: 
+    console.log("Age is 10");
+    break;
+  case 20:
+    console.log("Age is 20");
+    break;
+  case 30: 
+    console.log("Age is 30");
+    break;
+  default:
+    console.log("Invalid age value");
+}
+
