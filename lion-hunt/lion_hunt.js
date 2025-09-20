44f6c85a8129a6f7a0f37659f147bd465f47739b
Sanket Pawar <pawarss1103@gmail.com>
2025-09-20 18:22:16 +0530

    Final code of lion hunt story

---
+++ lion-hunt/lion_hunt.js
@@ -0,0 +1,49 @@
+const testCase1 = "LZ";
+const testCase2 = "L Z";
+const testCase3 = "L ZL";
+
+const testCaseToUse = testCase3;
+const stringOfALionAZebraAPatch = testCaseToUse;
+
+let counterOfString = 0;
+let index = 0;
+let leastDistance = -1;
+let output = 101;
+
+while(counterOfString < stringOfALionAZebraAPatch.length) {
+
+  if(stringOfALionAZebraAPatch[counterOfString] === "L") {
+    index = counterOfString + 1;
+
+    while(index < stringOfALionAZebraAPatch.length){
+      if(stringOfALionAZebraAPatch[index] === "Z"){
+        leastDistance = (index - counterOfString) - 1;
+      } 
+      index++;
+    }
+
+  } else if(stringOfALionAZebraAPatch[counterOfString] === "Z") {
+    index = counterOfString + 1;
+
+    while(index < stringOfALionAZebraAPatch.length){
+      if(stringOfALionAZebraAPatch[index] === "L"){
+        leastDistance = (index - counterOfString) - 1;
+      } 
+      index++;
+    }
+
+    if(leastDistance < output)
+    {
+      output = leastDistance;
+    } 
+
+  }
+
+  counterOfString++;
+}
+
+if(output !== 101) {
+  console.log("Input: ", stringOfALionAZebraAPatch, "Output: ", output);
+} else {
+  console.log("Input: ", stringOfALionAZebraAPatch, "Output: -1");
+}
