beb5e5fc328e09e4bd29b4f0936e57de8e5adb7c
Sanket Pawar <pawarss1103@gmail.com>
2025-09-20 18:10:36 +0530

    Same logic which repeats for the length of the main string times.

--- lion-hunt/logic_of_lion_hunt.js
+++ lion-hunt/logic_of_lion_hunt.js
@@ -1,7 +1,92 @@
-const stringOfALionAZebraAPatch = "ZZL";
-const counterOfString = 0;
+const stringOfALionAZebraAPatch = "LLL";
+let counterOfString = 0;
 let index = 0;
 let leastDistance = -1;
+
+// 1st Iteration
+if(counterOfString < stringOfALionAZebraAPatch.length) {
+  if(stringOfALionAZebraAPatch[counterOfString] === "L") {
+
+    index = counterOfString + 1;
+
+    if(stringOfALionAZebraAPatch[index] === "Z"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+
+    if(stringOfALionAZebraAPatch[index] === "Z"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+    
+    if(stringOfALionAZebraAPatch[index] === "Z"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+} else if(stringOfALionAZebraAPatch[counterOfString] === "Z") {
+    index = counterOfString + 1;
+
+    if(stringOfALionAZebraAPatch[index] === "L"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+
+    if(stringOfALionAZebraAPatch[index] === "L"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+    
+    if(stringOfALionAZebraAPatch[index] === "L"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+  }
+}
+
+counterOfString++;
+
+// 2nd Iteration
+if(counterOfString < stringOfALionAZebraAPatch.length) {
+  if(stringOfALionAZebraAPatch[counterOfString] === "L") {
+
+    index = counterOfString + 1;
+
+    if(stringOfALionAZebraAPatch[index] === "Z"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+
+    if(stringOfALionAZebraAPatch[index] === "Z"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+    
+    if(stringOfALionAZebraAPatch[index] === "Z"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+} else if(stringOfALionAZebraAPatch[counterOfString] === "Z") {
+    index = counterOfString + 1;
+
+    if(stringOfALionAZebraAPatch[index] === "L"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+
+    if(stringOfALionAZebraAPatch[index] === "L"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+    
+    if(stringOfALionAZebraAPatch[index] === "L"){
+      leastDistance = (index - counterOfString) - 1;
+    } 
+    index++;
+  }
+}
+
+//  3ed Iteration
+counterOfString++;
 
 if(counterOfString < stringOfALionAZebraAPatch.length) {
   if(stringOfALionAZebraAPatch[counterOfString] === "L") {
@@ -42,5 +127,8 @@ if(counterOfString < stringOfALionAZebraAPatch.length) {
   }
 }
 
-console.log(leastDistance);
+counterOfString++;
 
+// So we can do same thing for length of main string times
+
+console.log(leastDistance);
