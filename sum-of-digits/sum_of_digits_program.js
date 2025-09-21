172179a78f5bfe6f1ee9b0003682c22756118803
Sanket Pawar <pawarss1103@gmail.com>
2025-09-21 15:52:41 +0530

    Final working code for calculating sum of digits in number.

--- sum-of-digits/sum_of_digits_program.js
+++ sum-of-digits/sum_of_digits_program.js
@@ -1,3 +1,16 @@
-const inputNumber = 5;
-const sumOfDigits = inputNumber;
+const inputNumber = 8743;
+let sumOfDigits = 0;
+let copyOfInputNumber = inputNumber;
+let lastBitOfCopyNumber;
+
+if(inputNumber !== undefined) {
+
+  while(copyOfInputNumber !== 0) {
+    lastBitOfCopyNumber = copyOfInputNumber % 10;
+    sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
+    copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
+  }
+
+}
+
 console.log("Input : ", inputNumber, "\nSum of digits : ", sumOfDigits);
