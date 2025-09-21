dd0aa6347972ca8389e2017263a0e15ac58529f4
Sanket Pawar <pawarss1103@gmail.com>
2025-09-21 15:36:09 +0530

    This is the final logic for sum of digits.
    Now, replacement of series of an if statements into looping block.

--- sum-of-digits/logic_of_sum_of_digits.js
+++ sum-of-digits/logic_of_sum_of_digits.js
@@ -1,11 +1,41 @@
-const inputNumber = 89;
+const inputNumber = 9854;
 let sumOfDigits = 0;
 let copyOfInputNumber = inputNumber;
+let lastBitOfCopyNumber;
 
-sumOfDigits = sumOfDigits + (copyOfInputNumber % 10);
-copyOfInputNumber = ((copyOfInputNumber - sumOfDigits) / 10);
+if(copyOfInputNumber !== 0) {
+  lastBitOfCopyNumber = copyOfInputNumber % 10;
+  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
+  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
+}
 
-sumOfDigits = sumOfDigits + (copyOfInputNumber % 10);
-copyOfInputNumber = ((copyOfInputNumber - sumOfDigits) / 10);
+if(copyOfInputNumber !== 0) {
+  lastBitOfCopyNumber = copyOfInputNumber % 10;
+  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
+  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
+}
 
-console.log("Input : ", inputNumber, "\nSum of digits : ", sumOfDigits);
+if(copyOfInputNumber !== 0) {
+  lastBitOfCopyNumber = copyOfInputNumber % 10;
+  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
+  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
+}
+
+if(copyOfInputNumber !== 0) {
+  lastBitOfCopyNumber = copyOfInputNumber % 10;
+  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
+  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
+}
+
+if(copyOfInputNumber !== 0) {
+  lastBitOfCopyNumber = copyOfInputNumber % 10;
+  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
+  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
+}
+
+if(inputNumber === undefined) {
+  sumOfDigits = 0;
+  console.log("Input : ", inputNumber, "\nSum of digits : ", sumOfDigits);
+} else {
+  console.log("Input : ", inputNumber, "\nSum of digits : ", sumOfDigits);
+}
