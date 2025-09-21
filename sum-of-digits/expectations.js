3d0b6cd90605008e1c4a2090bb9bc9b6093c1cd5
Sanket Pawar <pawarss1103@gmail.com>
2025-09-21 14:31:33 +0530

    File contains an input and an expected output combination's list.

---
+++ sum-of-digits/expectations.js
@@ -0,0 +1,27 @@
+# Input and expected output combinations for sum of digits
+
+Input        |      Expected output
+
+  0          |      Sum of digits is 0
+ 
+  1          |      Sum of digits is 1
+  
+  3          |      Sum of digits is 3
+  
+  7          |      Sum of digits is 7
+  
+  10         |      Sum of digits is 1
+  
+  25         |      Sum of digits is 7
+  
+  201        |      Sum of digits is 3
+  
+  999        |      Sum of digits is 27
+  
+  1234       |      Sum of digits is 10
+  
+  67956      |      Sum of digits is 33
+  
+  4738018    |      Sum of digits is 31
+  
+             |      Sum of digits is 0
