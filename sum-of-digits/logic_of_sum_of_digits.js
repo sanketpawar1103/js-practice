3694642250d253d9ce8f3f3bf4c85b2cd5a33bb6
Sanket Pawar <pawarss1103@gmail.com>
2025-09-21 15:49:46 +0530

    Final updated file
    In this updation, the series of an if statements is replaces by while loop along with condition. And most important updation is about empty(undefined) input value. Empty value is also giving an expected output.

--- sum-of-digits/logic_of_sum_of_digits.js
+++ sum-of-digits/logic_of_sum_of_digits.js
@@ -1,41 +1,16 @@
-const inputNumber = 9854;
+const inputNumber = 1290;
 let sumOfDigits = 0;
 let copyOfInputNumber = inputNumber;
 let lastBitOfCopyNumber;
 
-if(copyOfInputNumber !== 0) {
-  lastBitOfCopyNumber = copyOfInputNumber % 10;
-  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
-  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
+if(inputNumber !== undefined) {
+
+  while(copyOfInputNumber !== 0) {
+    lastBitOfCopyNumber = copyOfInputNumber % 10;
+    sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
+    copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
+  }
+
 }
 
-if(copyOfInputNumber !== 0) {
-  lastBitOfCopyNumber = copyOfInputNumber % 10;
-  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
-  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
-}
-
-if(copyOfInputNumber !== 0) {
-  lastBitOfCopyNumber = copyOfInputNumber % 10;
-  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
-  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
-}
-
-if(copyOfInputNumber !== 0) {
-  lastBitOfCopyNumber = copyOfInputNumber % 10;
-  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
-  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
-}
-
-if(copyOfInputNumber !== 0) {
-  lastBitOfCopyNumber = copyOfInputNumber % 10;
-  sumOfDigits = sumOfDigits + lastBitOfCopyNumber;
-  copyOfInputNumber = ((copyOfInputNumber - lastBitOfCopyNumber) / 10);
-}
-
-if(inputNumber === undefined) {
-  sumOfDigits = 0;
-  console.log("Input : ", inputNumber, "\nSum of digits : ", sumOfDigits);
-} else {
-  console.log("Input : ", inputNumber, "\nSum of digits : ", sumOfDigits);
-}
+console.log("Input : ", inputNumber, "\nSum of digits : ", sumOfDigits);
