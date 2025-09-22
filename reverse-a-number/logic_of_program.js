6177c85b48d3c78294622d3dec5e7fdf31b04017
Sanket Pawar <pawarss1103@gmail.com>
2025-09-22 10:14:45 +0530

    Final logic in the series of if statements.

--- reverse-a-number/logic_of_program.js
+++ reverse-a-number/logic_of_program.js
@@ -1,17 +1,21 @@
-const inputNumber = 89;
+const inputNumber = 80;
 let reversedInputNumber = 0;
 let copyOfAnInputNumber = inputNumber;
 let multiplyingReversedNumber;
 let lastBitOfCopy;
 
-lastBitOfCopy = copyOfAnInputNumber % 10;
-multiplyingReversedNumber = reversedInputNumber * 10
-reversedInputNumber = multiplyingReversedNumber + lastBitOfCopy;
-copyOfAnInputNumber = (copyOfAnInputNumber - lastBitOfCopy) / 10;
+if(copyOfAnInputNumber !== 0){
+  lastBitOfCopy = copyOfAnInputNumber % 10;
+  multiplyingReversedNumber = reversedInputNumber * 10
+  reversedInputNumber = multiplyingReversedNumber + lastBitOfCopy;
+  copyOfAnInputNumber = (copyOfAnInputNumber - lastBitOfCopy) / 10;
+}
 
-lastBitOfCopy = copyOfAnInputNumber % 10;
-multiplyingReversedNumber = reversedInputNumber * 10
-reversedInputNumber = multiplyingReversedNumber + lastBitOfCopy;
-copyOfAnInputNumber = (copyOfAnInputNumber - lastBitOfCopy) / 10;
+if(copyOfAnInputNumber !== 0){
+  lastBitOfCopy = copyOfAnInputNumber % 10;
+  multiplyingReversedNumber = reversedInputNumber * 10
+  reversedInputNumber = multiplyingReversedNumber + lastBitOfCopy;
+  copyOfAnInputNumber = (copyOfAnInputNumber - lastBitOfCopy) / 10;
+}
 
 console.log("Input Number :", inputNumber, "\nReversed Number :", reversedInputNumber);
