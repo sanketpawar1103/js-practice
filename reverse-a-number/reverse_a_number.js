9f151f2ea18368d5fa90c0a5bdd4360f014cf28a
Sanket Pawar <pawarss1103@gmail.com>
2025-09-22 10:17:07 +0530

    Final working draft.

--- reverse-a-number/reverse_a_number.js
+++ reverse-a-number/reverse_a_number.js
@@ -1,4 +1,14 @@
-const inputNumber = 7;
-let reversedInputNumber = inputNumber;
+const inputNumber = 8237459;
+let reversedInputNumber = 0;
+let copyOfAnInputNumber = inputNumber;
+let multiplyingReversedNumber;
+let lastBitOfCopy;
+
+while(copyOfAnInputNumber !== 0){
+  lastBitOfCopy = copyOfAnInputNumber % 10;
+  multiplyingReversedNumber = reversedInputNumber * 10
+  reversedInputNumber = multiplyingReversedNumber + lastBitOfCopy;
+  copyOfAnInputNumber = (copyOfAnInputNumber - lastBitOfCopy) / 10;
+}
 
 console.log("Input Number :", inputNumber, "\nReversed Number :", reversedInputNumber);
