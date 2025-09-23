52d7a454725341f9e0c85a3e26a14a3af82cc817
Sanket Pawar <pawarss1103@gmail.com>
2025-09-23 18:31:03 +0530

    Added a function which performs addition of 2 numbers.

--- function-practice/display_number.js
+++ function-practice/display_number.js
@@ -3,5 +3,12 @@ function displayNumber(number) {
 }
 
 const numberToDisplay = displayNumber(10);
-
 console.log(numberToDisplay);
+
+//Addition of 2 number
+function performAddition(operand1, operand2) {
+  return operand1 + operand2;
+}
+
+const resultOfAddition = performAddition(10, 20);
+console.log(resultOfAddition);
