0724092f7c4d8fa02d24022989ba9d04652d8674
Sanket Pawar <pawarss1103@gmail.com>
2025-09-23 18:39:02 +0530

    Added functions for remaining arithmetic operations.

--- function-practice/display_number.js
+++ function-practice/display_number.js
@@ -12,3 +12,27 @@ function performAddition(operand1, operand2) {
 
 const resultOfAddition = performAddition(10, 20);
 console.log(resultOfAddition);
+
+// Substraction operation
+function performSubstraction(operand1, operand2) {
+  return operand1 - operand2;
+}
+
+const resultOfSubstraction = performSubstraction(10, 20);
+console.log(resultOfSubstraction);
+
+// Multiplication operation
+function performMultiplication(operand1, operand2) {
+  return operand1 * operand2;
+}
+
+const resultOfMultiplication = performMultiplication(10, 20);
+console.log(resultOfMultiplication);
+
+// Devision operation
+function performDivision(operand1, operand2) {
+  return operand1 / operand2;
+}
+
+const resultOfDivision = performDivision(10, 20);
+console.log(resultOfDivision);
