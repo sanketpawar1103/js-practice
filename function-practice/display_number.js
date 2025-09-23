ce50d6d11a66dc17938b24758d9047ae0897dda1
Sanket Pawar <pawarss1103@gmail.com>
2025-09-23 18:59:53 +0530

    Added functions which helps to calculate greatest of three numbers.

--- function-practice/display_number.js
+++ function-practice/display_number.js
@@ -36,3 +36,21 @@ function performDivision(operand1, operand2) {
 
 const resultOfDivision = performDivision(10, 20);
 console.log(resultOfDivision);
+
+// Greatest of 2 numbers and 3 numbers
+function greatestOf2Numbers(firstNumber, secondNumber) {
+  return (firstNumber > secondNumber) ? firstNumber : secondNumber; 
+}
+
+function greatestOf3Numbers(firstNumber, secondNumber, thirdNumber) {
+  const firstComparison = greatestOf2Numbers(firstNumber, secondNumber);
+  return (firstComparison > secondNumber) ? firstComparison : thirdNumber;
+}
+
+const number1 = 10;
+const number2 = 10;
+const number3 = 30;
+
+const greatestNumber = greatestOf3Numbers(number1, number2, number3);
+
+console.log(greatestNumber);
