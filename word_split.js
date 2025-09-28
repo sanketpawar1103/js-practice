4e14e3d9040a013466a75a80aa2d14859847dd71
Sanket Pawar <pawarss1103@gmail.com>
2025-09-28 13:42:07 +0530

    In this commit, I am going to commit the logic of the program.
    The logic needs to be implemented in loop, I put it in series of an if-else block. For next commit, I will convert the series of an If-else block into loop.

--- word_split.js
+++ word_split.js
@@ -1,5 +1,71 @@
-function splitWord(word) {
-  return word;
+function isVowel(currentCharacter) {
+  const vowelString = "aeiou";
+  for (let term = 0; term < vowelString.length; term++) {
+    if (currentCharacter === vowelString[term]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function splitWord(word, splitedWord) {
+  let remainingChars = "";
+  let term = 0;
+  splitedWord += word[term];
+
+  term++;
+  if (isVowel(splitedWord[splitedWord.length - 1])) {
+    if (!isVowel(word[term])) {
+      splitedWord += word[term];
+    } else {
+      remainingChars += word[term];
+    }
+  } else {
+     if (isVowel(word[term])) {
+      splitedWord += word[term];
+    } else {
+      remainingChars += word[term];
+    }
+  } 
+
+  term++;
+  if (isVowel(splitedWord[splitedWord.length - 1])) {
+    if (!isVowel(word[term])) {
+      splitedWord += word[term];
+    } else {
+      remainingChars += word[term];
+    }
+  } else {
+     if (isVowel(word[term])) {
+      splitedWord += word[term];
+    } else {
+      remainingChars += word[term];
+    }
+  }
+
+  term++;
+  if (isVowel(splitedWord[splitedWord.length - 1])) {
+    if (!isVowel(word[term])) {
+      splitedWord += word[term];
+    } else {
+      remainingChars += word[term];
+    }
+  } else {
+     if (isVowel(word[term])) {
+      splitedWord += word[term];
+    } else {
+      remainingChars += word[term];
+    }
+  }
+
+  if (remainingChars.length > 0) {
+    splitedWord += ",";
+  }
+
+  console.log("Splited Word = ", splitedWord);
+  console.log("Remaining chars = ", remainingChars);
+
+  return splitedWord;
 }
 
 function composeMessage(word, actualOutput, expectedOutput) {
@@ -12,15 +78,14 @@ function composeMessage(word, actualOutput, expectedOutput) {
 }
 
 function testCase(word, expectedOutput) {
-  const actualOutput = splitWord(word);
+  const actualOutput = splitWord(word, "");
   composeMessage(word, actualOutput, expectedOutput);
 }
 
 function testAll() {
   testCase("abec", "abec");
-  testCase("abeca", "abeca");
-  testCase("abecad", "abecad");
-  testCase("aabc", "abac");
+  testCase("cabe", "cabe");
+  testCase("caex", "cax, e");
 }
 
 testAll();
