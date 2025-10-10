549fbc6a2bdacae044c777bcc2019543326d3df7
Sanket Pawar <pawarss1103@gmail.com>
2025-10-10 15:08:35 +0530

    Create number_guessing_game.js

---
+++ Applications/number_guessing_game.js
@@ -0,0 +1,36 @@
+function guessNumber() {
+  const gussedstring = prompt("Guess a Number :");
+  return parseInt(gussedstring);
+}
+
+function giveHint(randomNumber, guessedNumber) {
+  if (guessedNumber > randomNumber) {
+    return "Number is smaller";
+  }
+
+  return "Number is greater";
+}
+
+function play(randomNumber, chances) {
+  if (chances === 0) {
+    console.log("You lost.!!!\nRandom Number = " + randomNumber);
+    return;
+  }
+
+  const guessedNumber = guessNumber();
+  if (guessedNumber === randomNumber) {
+    console.log("You won.!!!");
+    return;
+  }
+
+  console.log(giveHint(randomNumber, guessedNumber));
+
+  return play(randomNumber, chances - 1)
+}
+
+function generateRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 100);
+  play(randomNumber, 5);
+}
+
+generateRandomNumber();
