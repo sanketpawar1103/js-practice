20d8e1e7adfcfe4ed1ddd34f6c7045ee92f76a40
Sanket Pawar <pawarss1103@gmail.com>
2025-10-14 14:57:27 +0530

    Extended game version.
    It will not work, if you enter empty value in number of steps.

--- Applications/run_over_the_hole.js
+++ Applications/run_over_the_hole.js
@@ -1,31 +1,3 @@
-function manageGrid(pos) {
-  switch (pos[1]) {
-    case 1 : return main();
-  }
-
-  const mainGrid = createGritWithPlayer(pos);
-  return displayGrid(mainGrid);
-}
-
-function displayGrid(lines) {
-  for (let index = 0; index < lines.length; index++) {
-    console.log(lines[index].join(""));
-  }
-
-  return;
-}
-
-function readStepsCount() {
-  const noOfStepsStr = prompt("Enter number of steps (1 / 2) :");
-  const noOfStepsInt = parseInt(noOfStepsStr);
-
-  if (noOfStepsInt > 2) {
-    return main();
-  }
-
-  return noOfStepsInt;
-}
-
 function createGritWithPlayer(pos) {
   let mainGrid = [];
 
@@ -34,6 +6,7 @@ function createGritWithPlayer(pos) {
     for (let col = 0; col < 3; col++) {
       innerArray.push("⬜️");
     }
+
     mainGrid.push(innerArray);
   }
 
@@ -41,13 +14,55 @@ function createGritWithPlayer(pos) {
   return mainGrid;
 }
 
-function playGame(pos, numberOfMoves) {
-  if (numberOfMoves >= 17) {
+function displayGrid(mainGrid) {
+  for (let index = 0; index < mainGrid.length; index++) {
+    console.log(mainGrid[index].join(""));
+  }
+}
+
+function manageGrid(pos) {
+  const mainGrid = createGritWithPlayer(pos);
+  displayGrid(mainGrid);
+}
+
+function isHole(pos) {
+  switch (pos[1]) {
+    case 0: return false;
+    case 1: return true;
+    case 2: return false;
+  }
+}
+
+function readStepsCount(noOfMoves) {
+  const noOfStepsStr = prompt("Enter number of steps (1 / 2) :");
+  const noOfStepsInt = parseInt(noOfStepsStr);
+
+  if (noOfStepsInt > 2 || noOfStepsInt === undefined) {
+    return main(noOfMoves + 1);
+  }
+
+  return noOfStepsInt;
+}
+
+function playGame(pos, currentStepNum, noOfMoves) {
+  if (currentStepNum >= 17) {
     return "Congratulations.\nYou reach the";
   }
 
-  const noOfSteps = readStepsCount();
+  const noOfSteps = readStepsCount(noOfMoves);
   console.clear();
+  console.log(`Number of moves : ${noOfMoves}`);
+  const updatedPosition = updatePlayersPosition (pos, noOfSteps);
+
+  if (isHole(updatedPosition)) {
+    return main(noOfMoves + 1);
+  }
+
+  manageGrid(updatedPosition);
+  return playGame(updatedPosition, currentStepNum + noOfSteps, noOfMoves + 1);
+}
+
+function updatePlayersPosition(pos, noOfSteps) {
   pos[1] = pos[1] + noOfSteps;
 
   if (pos[1] >= 3) {
@@ -55,14 +70,13 @@ function playGame(pos, numberOfMoves) {
     pos[1] = pos[1] % 3;
   }
 
-  manageGrid(pos);
-  return playGame([pos[0], pos[1]], numberOfMoves + noOfSteps);
+  return pos;
 }
 
-function main() {
-  const mainGrid = createGritWithPlayer([0, 0]);
-  displayGrid(mainGrid);
-  console.log(playGame([0, 0], 1));
+function main(noOfMoves = 1, pos = [0, 0]) {
+  console.clear();
+  manageGrid(pos);
+  console.log(playGame([0, 0], 1, noOfMoves));
   return;
 }
 
