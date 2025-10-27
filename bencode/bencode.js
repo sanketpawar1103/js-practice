f7f2c1fb8e6b99abc58be29c179622b9b1e0ce35
Sanket Pawar <pawarss1103@gmail.com>
2025-10-27 15:46:45 +0530

    Now the functionality of bencode is extended. It can handle both, integer value as well as string values.

--- bencode/bencode.js
+++ bencode/bencode.js
@@ -1,6 +1,8 @@
 function encode(dataPacket) {
-  const encodedInteger = `i${dataPacket}e`;
-  return encodedInteger;
+  switch(typeof dataPacket) {
+    case "string": return `${dataPacket.length}:${dataPacket}`;
+    case "number": return `i${dataPacket}e`;
+  }
 }
 
 function composeMessage(description, actualResult, expectedResult, input) {
@@ -24,8 +26,7 @@ function testCase(description, dataPacket, expected) {
   composeMessage(description, actual, expected, dataPacket);
 }
 
-function testInteger() {
-  console.log(`\n${"-".repeat(50)}`);
+function testIntegers() {
   console.log("Test only integer values as an input :\n");
   testCase("Simple one digit integer", 5, 'i5e');
   testCase("Two digit positive integer", 50, 'i50e');
@@ -35,8 +36,21 @@ function testInteger() {
   console.log("-".repeat(50));
 }
 
+function testStrings() {
+  console.log("Test only string values as an input :\n");
+  testCase("Simple string value", "abc", "3:abc");
+  testCase("String with similar chars", "aaa", "3:aaa");
+  testCase("Empty string", "", "0:");
+  testCase("String with integers", "abc123", "6:abc123");
+  testCase("String with spacial chars", "SpecialChars!@#$%", "17:SpecialChars!@#$%");
+  testCase("Spaced string", "     ", "5:     ");
+  console.log("-".repeat(50));
+}
+
 function testAll() {
-  testInteger();
+  console.log(`\n${"-".repeat(50)}`);
+  testIntegers();
+  testStrings();
 }
 
 testAll();
