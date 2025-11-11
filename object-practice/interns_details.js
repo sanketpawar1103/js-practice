d980cd0ab1b30976d567156e9af3d4d87c23863f
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-11-11 18:09:07 +0530

    Fetch the details of interns from Kerala state

---
+++ object-practice/interns_details.js
@@ -0,0 +1,18 @@
+const INTERNS = [
+  { name: 'Sanket Pawar', age: 19, state: 'MH' },
+  { name: 'Gopi Vinay', age: 20, state: 'AP' },
+  { name: 'Pradipta Dey', age: 19, state: 'WB' },
+  { name: 'Vivek Bharadwaj', age: 21, state: 'UP' },
+  { name: 'Deepak Bahuguna', age: 22, state: 'UK' },
+  { name: 'Dinesh Kumar', age: 20, state: 'Delhi' },
+  { name: 'Gautham Krishna', age: 20, state: 'Kerala' },
+  { name: 'Hem Pradeep', age: 20, state: 'AP' },
+  { name: 'Rahul Vishwakarma', age: 22, state: 'UP' },
+  { name: 'Sagnik Ghosh', age: 20, state: 'WB' },
+  { name: 'Santo K', age: 20, state: 'Kerala' },
+  { name: 'Ayush Varma', age: 19, state: 'Delhi' },
+  { name: 'Yash Gairola', age: 20, state: 'UK' },
+];
+
+// Display the details of interns from Kerala
+console.log(INTERNS.filter(i => i.state === 'Kerala'));
