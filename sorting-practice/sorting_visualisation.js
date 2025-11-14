7802acaaba8c1b193c81fe911865d8a9d0b8cfc9
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-11-14 18:44:50 +0530

    Simple visualisation of selection sort

--- sorting-practice/sorting_visualisation.js
+++ sorting-practice/sorting_visualisation.js
@@ -1,21 +1,36 @@
 console.log("Selection sort..");
 
+const fakeDelay = () => {
+  for (let index = 0; index < 1800000000; index++) {}
+};
+
 const swapper = (array, i, j) => {
   const numHolder = array[i];
   array[i] = array[j];
   array[j] = numHolder;
 };
 
+const visualiser = (array) => {
+  for (let index = 0; index < array.length; index++) {
+    console.log(`${array[index] + "⬜️".repeat(array[index])}`);
+  }
+};
+
 const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
+      console.clear();
+
       if (array[i] > array[j]) {
         swapper(array, i, j);
       }
+
+      visualiser(array);
+      fakeDelay();
     }
   }
 
   return array;
 };
 
-console.log(selectionSort([3, 5, 1, 6, 2]));
+console.log(selectionSort([3, 5, 1, 6]));
