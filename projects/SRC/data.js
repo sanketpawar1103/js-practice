4f6f23ea24c7c2646533ca463297717178b7d24b
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-11-15 23:39:10 +0530

    User login functionality

--- projects/SRC/data.js
+++ projects/SRC/data.js
@@ -3,14 +3,17 @@ export const accHolders = [{
   accNo: 93939393,
   balance: 10000,
   pin: 4912,
+  password: 1234,
 }, {
   name: "Pradipta Dey",
   accNo: 92929292,
   balance: 10000,
   pin: 2006,
+  password: 1234,
 }, {
   name: "Gopi Vinay",
   accNo: 91919191,
   balance: 10000,
   pin: 1234,
+  password: 1234,
 }];
