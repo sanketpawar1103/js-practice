da2cd0e401770f7001fe3c5403d4e08fed2cf7b5
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-11-15 22:15:07 +0530

    Dummy data of users

---
+++ projects/SRC/data.js
@@ -0,0 +1,16 @@
+export const accHolders = [{
+  name: "Sanket Pawar",
+  accNo: 93939393,
+  balance: 10000,
+  pin: 4912,
+}, {
+  name: "Pradipta Dey",
+  accNo: 92929292,
+  balance: 10000,
+  pin: 2006,
+}, {
+  name: "Gopi Vinay",
+  accNo: 91919191,
+  balance: 10000,
+  pin: 1234,
+}];
