875b29c6f64aaed4a86bc6fb749feff43fbe3741
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-12-01 12:20:25 +0530

    Multiple strings in a single array

--- patterns/marquee.js
+++ patterns/marquee.js
@@ -1,38 +1,44 @@
 const grid = [];
-let [text, row, col, inc] = ["Sanket", 0, 0, 0];
-let [text2, row2, col2, dec] = ["Vinay", 2, 35, 35];
 
-const clearGrid = () => {
+const createGrid = () => {
+  console.clear();
   for (let index = 0; index < 5; index++) {
     grid[index] = " ".repeat(40).split("");
   }
 };
 
 const screen = {
-  toLeft: () => {
-    col2 = dec;
-    for (let index = 0; index < text2.length; index++) {
-      grid[row2][col2] = text2[index];
-      col2 = (col2 + 1) % 40;
+  toLeft: (info) => {
+    info.change = info.change < 1 ? 40 : info.change - 1;
+    let col = info.change;
+    for (let index = 0; index < info.text.length; index++) {
+      grid[info.row][col] = info.text[index];
+      col = (col + 1) % 40;
     }
   },
 
-  toRight: () => {
-    col = inc;
-    for (let index = 0; index < text.length; index++) {
-      grid[row][col] = text[index];
+  toRight: (info) => {
+    info.change = (info.change + 1) % 40;
+    let col = info.change;
+    for (let index = 0; index < info.text.length; index++) {
+      grid[info.row][col] = info.text[index];
       col = (col + 1) % 40;
     }
   },
 };
 
+const data = [
+  { text: "Sanket", row: 0, change: 0, animate: screen.toRight },
+  { text: "Pawar", row: 3, change: 35, animate: screen.toLeft },
+];
+
+
 setInterval(() => {
-  console.clear();
-  clearGrid();
-  inc = (inc + 1) % 40;
-  dec = dec < 1 ? 40 : dec - 1;
-  screen.toRight();
-  screen.toLeft();
+  createGrid();
+
+  data.forEach(info => {
+    info.animate(info);
+  });
 
   for (let index = 0; index < grid.length; index++) {
     grid[index] = grid[index].join("");
