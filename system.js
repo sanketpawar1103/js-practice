f29add940805aaf741ee5cb8d17b690a7db8a21b
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-12-25 10:03:27 +0530

    Adds login functionality

--- system.js
+++ system.js
@@ -1,16 +1,26 @@
+import { promptSecret } from "@std/cli";
+
+const displayOptions = (str) => {
+  console.log(str);
+};
+
 const isInValidUser = (user) => {
+  console.log(user);
   console.clear();
-  if (!user.name && user.name.incluces(",")) {
+  if (!user.name && user.name.includes(",")) {
     console.log(" Empty name Or remove ,\n");
     return true;
-  } else if (user.phone.length !== 10 && user.phone.incluces(",")) {
+  } else if (user.phone.length !== 10 || user.phone.includes(",")) {
     console.log(" Phone number should be of 10 digits Or remove ,\n");
     return true;
-  } else if (user.pass.length < 4 && user.pass.incluces(",")) {
+  } else if (user.pass.length < 4 || user.pass.includes(",")) {
     console.log(" Password should be atleast of 4 charachters remove ,\n");
     return true;
-  } else if (user.pin.length !== 4 && user.pin.incluces(",")) {
+  } else if (user.pin.length !== 4 || user.pin.includes(",")) {
     console.log(" Pin should be of 4 chars only remove ,\n");
+    return true;
+  } else if (user.balance || user.balance <= 0) {
+    console.log("Invalid balance amount, \n");
     return true;
   }
 
@@ -19,25 +29,62 @@ const isInValidUser = (user) => {
 
 const readUserDetails = () => {
   console.clear();
+  console.log(" | CREATE ACCOUNT |\n");
   const name = prompt("Enter full name:");
   const phone = prompt("Enter phone number:");
-  const pass = prompt("Enter password for account:");
-  const pin = prompt("Enter pin:");
+  const balance = prompt("Enter balance:");
+  const pass = promptSecret("Enter password for account:");
+  const pin = promptSecret("Enter pin:");
 
-  return { name, phone, pass, pin };
+  return { name, phone, balance, pass, pin };
 };
 
 const readCredentials = () => {
   console.clear();
   const phone = prompt("Enter mobile number:");
-  const pass = prompt("Enter password :");
+  const pass = promptSecret("Enter password :");
 
-  return [phone, pass];
+  return { phone, pass };
 };
 
+const formatUserData = () => {
+  const users = [];
+  Deno.readTextFileSync("./data.csv")
+    .split("\n")
+    .forEach((each) => {
+      const user = each.split(",");
+      users.push({
+        name: user[0],
+        phone: user[1],
+        pass: user[2],
+        pin: user[3],
+        history: user.slice(4),
+      });
+    });
+
+  return users;
+};
+
+const authenticateUser = (credentials, users) => {
+  console.clear();
+
+  return users.find((each) =>
+    each.phone === credentials.phone && each.pass === credentials.pass
+  );
+};
 const logIn = () => {
-  const data = Deno.readTextFileSync("./data.csv").split("\n");
+  const users = formatUserData();
+  console.log(" | LOG IN PAGE |");
+
   const credentials = readCredentials();
+
+  if (!authenticateUser(credentials, users)) {
+    console.clear();
+    console.log("Credentials missmatch");
+    main();
+  }
+
+  console.log("Welcome");
 };
 
 const createAccount = () => {
@@ -46,6 +93,8 @@ const createAccount = () => {
   if (isInValidUser(user)) {
     return main();
   }
+
+  console.log(" Account created successfylly");
 
   Deno.writeTextFileSync("./data.csv", Object.values(user).join(","), {
     append: true,
