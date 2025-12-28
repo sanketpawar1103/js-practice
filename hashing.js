1ccd4aef7a57fb7180301bfb8cc8a7e68d52d29b
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-12-28 16:22:19 +0530

    adds nested array structure for hashing

--- hashing.js
+++ hashing.js
@@ -1,18 +1,17 @@
+const MSG = {
+  true: `Element Found `,
+  false: `Element not Found `,
+};
+
 const searchArrayElements = (array) => {
-  for (let term = 1000; term > 0; term--) {
+  for (let term = 200; term > 90; term--) {
     let index = 0;
 
     while (array[index] !== term && index < array.length) {
       index++;
     }
 
-    let msg = `Element not Found : ${term}`;
-
-    if (index < array.length) {
-      msg = `Element Found : ${term}`;
-    }
-
-    console.log(msg);
+    console.log(MSG[index < array.length], term);
   }
 };
 
@@ -26,35 +25,57 @@ const fillArray = () => {
   return array;
 };
 
+const changeArray = (element, term) => {
+  if (Array.isArray(element)) {
+    element[element.length] = term;
+  } else {
+    element = [element, term];
+  }
+
+  return element;
+};
+
+const isPresent = {
+  true: (element, term) => changeArray(element, term),
+  false: (_element, term) => term,
+};
+
 const fillTable = () => {
   const array = [];
 
   for (let term = 101; term <= 200; term++) {
-    array[term % 100] = term;
+    const address = term % 10;
+    const isNotUndefined = array[address] !== undefined;
+
+    array[address] = isPresent[isNotUndefined](array[address], term);
   }
 
   return array;
 };
 
 const searchTableElement = (array) => {
-  for (let term = 1000; term > 0; term--) {
-    let msg = `Element not Found : ${term}`;
-    const index = term % 100;
+  for (let term = 200; term > 90; term--) {
+    const index = term % 10;
+    let isTableElement = false;
 
-    if (term === array[index]) {
-      msg = `Element Found : ${term}`;
+    if (Array.isArray(array[index])) {
+      isTableElement = array[index].some((each) => each === term);
+    } else if (array[index] === term) {
+      isTableElement = true;
     }
 
-    console.log(msg);
+    console.log(MSG[isTableElement] + term);
   }
 };
 
 const main = () => {
   const startTimeOfArrayOperation = performance.now();
   const filledArray = fillArray();
+  console.log(filledArray);
   searchArrayElements(filledArray);
   const endTimeOfArrayOperation = performance.now();
   const filledTable = fillTable();
+  console.log(filledTable);
   searchTableElement(filledTable);
   const endTimeOfTableOperation = performance.now();
 
